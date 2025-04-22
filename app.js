ffe354f1d1fffa9c9c5c7e0cc6972f583a1df9ce
Gasan <gasanalunkachev@gmail.com>
2025-04-22 20:25:39 +0300

    Обновил проект: добавлен akhi.js, обновлены стили и скрипты

---
+++ app.js
@@ -0,0 +1,15 @@
+const inputNode = document.querySelector('.input-node')
+
+const btnNode = document.querySelector('.btn-node')
+
+
+btnNode.addEventListener('click', e => {
+    e.preventDefault()
+if(inputNode.value === ''){
+    alert('Please enter a valid input')
+}
+})
+
+
+
+
